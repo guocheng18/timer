d54069f305a0e43b0018dd0d8fcb64a165db8f88
Cheng Guo <guochengrz@foxmail.com>
2020-04-27 19:55:39 +0800

    complete project setup

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -4,9 +4,9 @@ from PyQt5.QtWidgets import QMainWindow
 import sys
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
+    appctxt = ApplicationContext()  # 1. Instantiate ApplicationContext
     window = QMainWindow()
     window.resize(250, 150)
     window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    exit_code = appctxt.app.exec_()  # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
